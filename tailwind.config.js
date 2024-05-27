7d46cad597bbd8b1f86e2b447f8682e0f8f99c8c
ekorahy <ekorahy@gmail.com>
2024-05-28 06:45:33 +0700

    feat: add empty directory for src and set color and font in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      primary: '#475569',
+      onPrimary: '#f8fafc',
+      secondary: '#cbd5e1',
+      onSecondary: '#020617',
+      body: '#0f172a',
+      title: '#020617',
+      dark: '#020617',
+      light: '#f8fafc',
+    },
+    fontFamily: {
+      railway: ['Raleway', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
